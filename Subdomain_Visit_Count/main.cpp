00967274133ffb7ef8eec7431162eae0f3f3c008
Jerson David Mendoza Alvarez <jdmendozaa@unal.edu.co>
2022-04-21 00:22:06 -0500

    Add some exercices

--- Subdomain_Visit_Count/main.cpp
+++ Subdomain_Visit_Count/main.cpp
@@ -33,10 +33,8 @@ public:
 
 
 int main() {
-
     auto s = new Solution();
 
     vector<string> cpdomains = {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"};
     s->subdomainVisits(cpdomains);
-
 }
